08071aa4ba3a6b3041a44ea5542ccc2231965170
Kate Ramsey <kateramsey@live.com>
2016-04-13 15:47:29 -0500

    Update RouteConfig to direct to Posts/Index. Deleted home controller and views

--- Readit/App_Start/RouteConfig.cs
+++ Readit/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Readit
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Posts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
