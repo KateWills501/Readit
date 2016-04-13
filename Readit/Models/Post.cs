312a3c51d57df33aece4f47a14df4f1b75649095
Kate Ramsey <kateramsey@live.com>
2016-04-13 14:12:15 -0500

    Initial commit with Post model

---
+++ Readit/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Readit.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+        [MaxLength(10000)]
+        public string Body { get; set; }
+        //[RegularExpression()]
+        public string ExternalLink { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+    }
+}
